61e3de667d33c09f777d053786c2e260b40c43a7
unknown <shahwazkhan041298@gmail.com>
2023-02-20 15:37:58 +0530

    working2

--- main.js
+++ main.js
@@ -19,16 +19,18 @@ function submitEvent(e){
     document.querySelector('#reload').style.display = 'block'
 
     let ul = document.createElement('div')
-
-
-
+    ul.className = ('m-2')
+    ul.style.border = '1px solid blue'
+    ul.style.borderRadius = ('0px 50px 50px 50px')
+    ul.style.padding = '15px'
+    ul.style.backgroundColor = ('aqua')
     outputInbox.appendChild(ul)
 
     let outputName = document.createElement('p')
     outputName.innerText = "Name : " + nameInput.value
     outputName.style.fontSize = ('18px')
-    outputName.style.marginTop =('50px')
-    // outputName.className = ('text-center')
+    // outputName.style.marginTop =('50px')
+    outputName.className = (' mt-2')
     ul.appendChild(outputName)
     // outputName.className = "list-group-item mb-2 p-2"
      
@@ -55,8 +57,11 @@ function submitEvent(e){
     // whatsApp.style.marginLeft = '20px'
     whatsApp.style.textDecoration = 'none'
     whatsApp.className = 'fa fa-whatsapp'
-    whatsApp.style.marginTop =('70px')
+    // whatsApp.style.marginTop =('70px')
     whatsApp.style.fontSize = '30px'
+    whatsApp.style.padding = '8px'
+    whatsApp.style.border = '1px solid green'
+    whatsApp.style.borderRadius = '30%'
     whatsApp.style.color = "green"
     whatsApp.setAttribute("href", "https://wa.me/" + mobileInput.value) 
     ul.appendChild(whatsApp)
@@ -64,9 +69,12 @@ function submitEvent(e){
 
     let callMe = document.createElement('a')
     callMe.className = 'fa fa-phone'
-    callMe.style.marginTop =('70px')
+    // callMe.style.marginTop =('70px')
     callMe.style.marginLeft = '50px'
     callMe.style.textDecoration = 'none'
+    callMe.style.padding = '8px'
+    callMe.style.border = '1px solid blue'
+    callMe.style.borderRadius = '30%'
     callMe.style.fontSize = '30px'
     callMe.style.color = 'blue'
     callMe.setAttribute("href", "tel:" + mobileInput.value  )
